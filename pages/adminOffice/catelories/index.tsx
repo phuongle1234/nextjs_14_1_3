e1c5e1c653ec427a0ce259cbc583f9b7027388b0
DavidPhuong98 <105845636+DavidPhuong98@users.noreply.github.com>
2024-06-24 17:01:25 +0700

    build

--- pages/adminOffice/catelories/index.tsx
+++ pages/adminOffice/catelories/index.tsx
@@ -73,7 +73,9 @@ const Child = ({ ...props }: any) => {
 
                             <td className="align-middle"  >
                                 <div className="btn" style={style} >
-                                    Edit
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                    </svg>
                                 </div>
 
                             </td>
@@ -110,7 +112,13 @@ const Child = ({ ...props }: any) => {
                                         <tr>
                                             <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">ID</th>
                                             <th className="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
-                                            <th className="text-secondary opacity-7" style={{ width: "20px" }}></th>
+                                            <th className="text-secondary opacity-7" style={{ width: "20px" }}>
+                                            <div className="btn" >
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                                </svg>
+                                            </div>
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -135,7 +143,9 @@ const Child = ({ ...props }: any) => {
                                                     </td>
 
                                                     <td className="align-middle">
-                                                        Edit
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                                        </svg>
                                                     </td>
                                                 </tr>
                                              
@@ -154,38 +164,6 @@ const Child = ({ ...props }: any) => {
                 </div>
             </div>
            
-            {/* <footer className="footer py-4  ">
-                <div className="container-fluid">
-                    <div className="row align-items-center justify-content-lg-between">
-                        <div className="col-lg-6 mb-lg-0 mb-4">
-                            <div className="copyright text-center text-sm text-muted text-lg-start">
-                                © <script>
-                                    document.write(new Date().getFullYear())
-                                </script>,
-                                made with <i className="fa fa-heart"></i> by
-                                <a href="https://www.creative-tim.com" className="font-weight-bold" target="_blank">Creative Tim</a>
-                                for a better web.
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <ul className="nav nav-footer justify-content-center justify-content-lg-end">
-                                <li className="nav-item">
-                                    <a href="https://www.creative-tim.com" className="nav-link text-muted" target="_blank">Creative Tim</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="https://www.creative-tim.com/presentation" className="nav-link text-muted" target="_blank">About Us</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="https://www.creative-tim.com/blog" className="nav-link text-muted" target="_blank">Blog</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a href="https://www.creative-tim.com/license" className="nav-link pe-0 text-muted" target="_blank">License</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </footer> */}
         </>
     )
 }
